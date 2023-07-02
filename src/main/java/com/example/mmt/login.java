0b80a58cc5c100de654e2b223da58f71187b1632
l0ksh <lokesh904@gmail.com>
2023-07-02 22:40:10 +0530

    Initial commit

---
+++ src/main/java/com/example/mmt/login.java
@@ -0,0 +1,13 @@
+package com.example.mmt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class login {
+    @GetMapping("/login")
+    public String getData(){
+        return "This is Login Page";
+    }
+
+}
